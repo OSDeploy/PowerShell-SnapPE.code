9da9a92b48f5ce0aae9055c4b038c4935747d726
David Segura <david@segura.org>
2018-11-05 00:30:42 -0600

    18.11.5

---
+++ Build.ps1
@@ -0,0 +1,15 @@
+﻿$title = "SnapPE"
+$description = "WinPE Screenshots"
+$company = "OSDeploy.com"
+$product = "SnapPE"
+$copyright = "2018 David Segura @SeguraOSD"
+$trademark = "OSDeploy.com"
+$version = "18.11.5.0"
+
+$iconFile = "$PSScriptRoot\ico\Powershell.ico"
+
+#$ps2exe = "$PSScriptRoot\ps2exe\ps2exe.ps1"
+$source = "$PSScriptRoot\SnapPE.ps1"
+$snappe = "$PSScriptRoot\SnapPE.exe"
+
+.\ps2exe\ps2exe.ps1 $source $snappe -Verbose -noConsole -title $title -description $description -company $company -product $product -copyright $copyright -trademark $trademark -version $version -iconFile $iconFile
